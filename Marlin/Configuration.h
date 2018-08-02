a7a1875bd46bdd90a32abf6495f4aef55a768bd5
FromtonRouge <fromtonrouge@gmail.com>
2018-08-03 00:22:28 +0200

    Better X, Y calibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,7 +529,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.72, 78.65, 399.29, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.87, 79.45, 399.29, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
