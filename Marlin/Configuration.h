15a6b283774c28822d5b248dea37e17489ca3aec
FromtonRouge <fromtonrouge@gmail.com>
2018-08-10 17:15:50 +0200

    X steps calibration again...

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -598,7 +598,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.54, 80, 399.29, 408.16 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.72, 80, 399.29, 408.16 }
 
 /**
  * Default Max Feed Rate (mm/s)
